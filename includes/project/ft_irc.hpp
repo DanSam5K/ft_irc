2d782c6f4de143a67b0112adc5d91316b2b9b1fa
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2025-08-29 16:37:52 +0200

    update changes

--- includes/project/ft_irc.hpp
+++ includes/project/ft_irc.hpp
@@ -51,7 +51,7 @@ std::string stringToLowercase(std::string string);
 
 // Returns true if elem is found in the list
 template <typename T>
-bool is_in_list(const std::list<T>& list, const T& elem)
+bool is_in_list(std::list<T>& list, T& elem)
 {
     for (typename std::list<T>::const_iterator it = list.begin(); it != list.end(); ++it)
     {
@@ -74,7 +74,7 @@ void delete_map(std::map<T, U>& map)
 
 // Checks if value exists in a static array
 template<typename T>
-bool is_in_array(const T& value, const T array[], unsigned int size_array)
+bool is_in_array(T& value, T array[], unsigned int size_array)
 {
     for (unsigned int i = 0; i < size_array; ++i)
     {
@@ -86,7 +86,7 @@ bool is_in_array(const T& value, const T array[], unsigned int size_array)
 
 // Returns the index of value in array, or -1 if not found
 template<typename T>
-unsigned int get_array_index(const T& value, const T array[], unsigned int size_array)
+unsigned int get_array_index(T& value, T array[], unsigned int size_array)
 {
     for (unsigned int i = 0; i < size_array; ++i)
     {
